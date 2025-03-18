3ca410067aa9f34f470c1b7775cfc46742837865
iiX0Lords <benjamin.short19@gmail.com>
2025-03-18 23:37:17 +1100

    loading and saving maps

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import eng
 import math
 import json
 import os
+import tkinter
+import tkinter.filedialog
 
 pygame.init()
+
+pygame.display.set_caption("Map qEditor")
+pygame.display.set_icon(pygame.image.load("icon.png"))
 
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
@@ -14,6 +19,16 @@ dt = 0
 
 preview = pygame.Surface((50, 50))
 
+def prompt_file(name = "exportedMap", write = False):
+    top = tkinter.Tk()
+    top.withdraw()
+    file_name = None
+    if write:
+        file_name = tkinter.filedialog.asksaveasfile(mode="w", initialfile = name+".json",filetypes = (("JSON files","*.json"),("All Files","*.*")),parent=top)
+    else:
+        file_name = tkinter.filedialog.askopenfile(mode="r",filetypes = (("JSON files","*.json"),("All Files","*.*")),parent=top)
+    top.destroy()
+    return file_name
 
 def snap(x, mult):
     return math.floor((x / mult) + 0.5) * mult
@@ -28,8 +43,31 @@ def export():
             "Position": (object.Object.x, object.Object.y)
         })
 
-    with open("export.txt", "w") as file:
-        file.write(json.dumps(exportArray))
+    file = prompt_file("exportedMap", True)
+    if file == None:
+        print("Didnt save")
+        return
+    file.write(json.dumps(exportArray))
+        
+
+def load(file = None):
+    if file == None:
+        file = prompt_file()
+        if file == None:
+            print("No file picked")
+            return
+        file = file.name
+    file = open(file, "r")
+    file = file.read()
+    openedArray = json.loads(file)
+
+    eng.workspace = []
+
+    for object in openedArray:
+        print(object)
+        newObject = eng.Object(pygame.Vector2(0, 0))
+        newObject.Object.update(object["Position"][0], object["Position"][1], 50, 50)
+    
 
 while running:
     screen.fill("black")
@@ -40,6 +78,10 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w:
                 export()
+            elif event.key == pygame.K_s:
+                load()
+            elif event.key == pygame.K_k:
+                load("export.json")
         if event.type == pygame.KEYUP:
             pass
 
